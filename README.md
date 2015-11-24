24501c1c5ac0f27fc0cb2e4a393bf00a23811e8b
Rutvi Patel <rutvi@uw.edu>
2015-11-23 22:05:42 -0800

    intitial Commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Personal-Portfolio
+
+What is the URL of this project on your student web-server?
+	students.washington.edu/rutvi/info343/Personal-Portfolio
+
+Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+	Nope
+
+Approximately how many hours did it take you to complete this challenge?
+	10 hours
+
+Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+	Nope. I tried really hard to use Angular Timeline, but even after installing I was unable to make certain requests go through
---
+++ js/app.js
@@ -0,0 +1,55 @@
+// Rutvi Patel
+// Personal Portfolio Challenge
+// 11/23/15
+// This page provides functions, and multipage architecture for the webpage
+
+// This intitializes the app that will allow for multipage arch
+var myApp = angular.module('myApp', ['ui.router'])
+// Config route provider
+.config(function($stateProvider) {
+    $stateProvider
+    // Each state routes to a different page
+  .state('home', {
+    url:'/',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeController',
+  })
+  .state('bio', {
+    url:'/bio',
+    templateUrl: 'templates/bio.html',
+    controller: 'bioController',
+  })
+  .state('projects', {
+    url:'/projects',
+    templateUrl: 'templates/projects.html',
+    controller: 'ProjectsController',
+  })
+})
+
+// All of the following controllers take in $scope so that
+// variables are visible in the html, and $http to request from 
+// the json file
+
+// controller for home page
+.controller('HomeController', function($scope, $http){
+  $http.get('json/home.json').success(function(data) {
+     $scope.homeOb = data;
+  })
+})
+
+// Controller for projects page
+.controller('ProjectsController', function($scope, $http){
+  $http.get('json/projects.json').success(function(data) {
+     $scope.projectsOb = data;
+  })
+})
+
+// Controller for biography page
+.controller('bioController', function($scope, $http){
+  $http.get('json/bio.json').success(function(data) {
+     $scope.bioObj = data;
+  })
+})
+
+
+
